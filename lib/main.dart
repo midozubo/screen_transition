8f0e43abd6d21047e75cf8aa22f684cd67414bed
asa_midozubo <136148216+midozubo@users.noreply.github.com>
2023-09-17 13:56:02 +0900

    Add files via upload
    ホーム画面を追加しましたが、シャッターボタンを押すとエラーが出ます

---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'screen0.dart';
+import 'screen1.dart';
+
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/', // 初期画面を'/'とする
+
+      title: 'screen transition',
+      home: MainPage(),
+      // routesで遷移元と遷移先のurlを設定する
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => MainPage(),
+        '/subpage': (BuildContext context) => TakePictureScreen(camera: camera),
+      },
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main Page')),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Main',
+                style: TextStyle(fontSize: 30),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(
+                //.MainPageの画面にSubPageの画面を上乗せする
+                onPressed: () => Navigator.of(context).pushNamed("/subpage"),
+                child: Text(
+                  'カメラを起動',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// 写真撮影画面
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      // カメラを指定
+      widget.camera,
+      // 解像度を定義
+      ResolutionPreset.medium,
+    );
+
+    // コントローラーを初期化
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // ウィジェットが破棄されたら、コントローラーを破棄
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+
+          Navigator.pushNamed(context, '/first');  // 移動したい画面の名前を設定する
+
+          // 写真を撮る
+          final image = await _controller.takePicture();
+          // 表示用の画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              fullscreenDialog: true,
+            ),
+          );
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+// 撮影した写真を表示する画面
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('撮れた写真')),
+      body: Center(child: Image.file(File(imagePath))),
+    );
+  }
+}
